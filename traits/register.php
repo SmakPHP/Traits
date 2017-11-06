2f8abce98088737cb743b265f3ad5200441a5d34
Andrey Masterov <apmasterov@gmail.com>
2017-11-06 18:49:31 +0300

    Фикс

--- traits/register.php
+++ traits/register.php
@@ -8,7 +8,7 @@ function register($class) {
     // Подключаем только свои библиотеки
     if (strpos($class, 'traits') === 0) {
         // Путь к подключаемому классу
-        $path = dirname(__DIR__).'/'.$class.'.php';
+        $path = dirname(__DIR__).'/'.str_replace('\\', '/', $class).'.php';
         // Если файл существует то загружаем
         if (file_exists($path)) require_once $path;
     }
